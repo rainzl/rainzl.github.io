0741f375369aa0033c1777620edbd6eb98dc322d
rainzl <369857686@qq.com>
2017-02-07 23:57:28 +0800

    27

--- js/move.js
+++ js/move.js
@@ -1,33 +1,41 @@
 var move = (function(){
 	var moveFn = {
 		Tween: {
+			//linear 匀速
 			linear: function (t, b, c, d){
 				return c*t/d + b;
 			},
+			//easeIn 加速
 			easeIn: function(t, b, c, d){
 				return c*(t/=d)*t + b;
 			},
+			//easeOut 减速
 			easeOut: function(t, b, c, d){
 				return -c *(t/=d)*(t-2) + b;
 			},
+			//easeBoth 先加速后减速
 			easeBoth: function(t, b, c, d){
 				if ((t/=d/2) < 1) {
 					return c/2*t*t + b;
 				}
 				return -c/2 * ((--t)*(t-2) - 1) + b;
 			},
+			//easeInStrong 二次方加速
 			easeInStrong: function(t, b, c, d){
 				return c*(t/=d)*t*t*t + b;
 			},
+			//easeOutStrong 二次方减速
 			easeOutStrong: function(t, b, c, d){
 				return -c * ((t=t/d-1)*t*t*t - 1) + b;
 			},
+			//easeBothStrong 二次方先加速后减速
 			easeBothStrong: function(t, b, c, d){
 				if ((t/=d/2) < 1) {
 					return c/2*t*t*t*t + b;
 				}
 				return -c/2 * ((t-=2)*t*t*t - 2) + b;
 			},
+			//elasticIn 弹性在开始方向
 			elasticIn: function(t, b, c, d, a, p){
 				if (t === 0) { 
 					return b; 
@@ -46,6 +54,7 @@ var move = (function(){
 				}
 				return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
 			},
+			//elasticout 弹性在结束方向
 			elasticOut: function(t, b, c, d, a, p){
 				if (t === 0) {
 					return b;
@@ -63,7 +72,8 @@ var move = (function(){
 					var s = p/(2*Math.PI) * Math.asin (c/a);
 				}
 				return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-			},    
+			},   
+			//elasticBoth 弹性 开始和结束都有
 			elasticBoth: function(t, b, c, d, a, p){
 				if (t === 0) {
 					return b;
@@ -88,18 +98,21 @@ var move = (function(){
 				return a*Math.pow(2,-10*(t-=1)) * 
 						Math.sin( (t*d-s)*(2*Math.PI)/p )*0.5 + c + b;
 			},
+			//backIn 回弹 在开始方向
 			backIn: function(t, b, c, d, s){
 				if (typeof s == 'undefined') {
 				   s = 1.70158;
 				}
 				return c*(t/=d)*t*((s+1)*t - s) + b;
 			},
+			//backOut 回弹 在结束方向
 			backOut: function(t, b, c, d, s){
 				if (typeof s == 'undefined') {
 					s = 2.70158;  //回缩的距离
 				}
 				return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
 			}, 
+			//backBoth 回弹 开始和结束都有
 			backBoth: function(t, b, c, d, s){
 				if (typeof s == 'undefined') {
 					s = 1.70158; 
@@ -109,9 +122,11 @@ var move = (function(){
 				}
 				return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
 			},
+			//bounceIn  碰撞 在开始方向
 			bounceIn: function(t, b, c, d){
 				return c - Tween['bounceOut'](d-t, 0, c, d) + b;
 			},       
+			//bounceOut 碰撞 在结束方向
 			bounceOut: function(t, b, c, d){
 				if ((t/=d) < (1/2.75)) {
 					return c*(7.5625*t*t) + b;
@@ -122,6 +137,7 @@ var move = (function(){
 				}
 				return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
 			},      
+			//bounceBoth 碰撞 开始和结束都有
 			bounceBoth: function(t, b, c, d){
 				if (t < d/2) {
 					return Tween['bounceIn'](t*2, 0, c, d) * 0.5 + b;
